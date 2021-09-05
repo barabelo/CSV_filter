a97a3e9cea3d034e074f5203fe4eb97dbefb536e
Bruno <barabelo@hotmail.com>
2021-09-04 22:25:00 -0300

    Adição da classe ServiceGame.

---
+++ src/main/java/br/com/dominio/br/com/dominio/service/ServiceGame.java
@@ -0,0 +1,25 @@
+package br.com.dominio.br.com.dominio.service;
+
+import br.com.dominio.model.Jogo;
+import br.com.dominio.model.Plataformas;
+import br.com.dominio.model.Publishers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ServiceGame {
+    public static List<Jogo> getJogosPorPlataforma(List<Jogo> jogos, Plataformas platforma) {
+        List<Jogo> jogosPorPlataforma = jogos.stream().filter((jogo ->
+                jogo.getPlatform().equals(platforma.getValorNoCsv()))).collect(Collectors.toList());
+        return jogosPorPlataforma;
+    }
+
+    public static List<Jogo> getJogosPorPublisher(List<Jogo> jogos, Publishers publisher) {
+        List<Jogo> jogosPorPublisher = jogos.stream().filter((jogo ->
+                jogo.getPublisher().equals(publisher.getValorNoCsv()))).collect(Collectors.toList());
+
+        return jogosPorPublisher;
+    }
+}
+
+

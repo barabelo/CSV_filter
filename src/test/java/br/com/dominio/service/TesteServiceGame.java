1a5391a702aa7cdbcc366f1fe8f6c0ce0d0a418b
Bruno <barabelo@hotmail.com>
2021-09-04 23:45:54 -0300

    Adição da classe TesteServiceGame

---
+++ src/test/java/br/com/dominio/service/TesteServiceGame.java
@@ -0,0 +1,37 @@
+package br.com.dominio.service;
+
+import br.com.dominio.model.Jogo;
+import br.com.dominio.model.Plataformas;
+import br.com.dominio.model.Publishers;
+import br.com.dominio.utils.OperacoesCsv;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class TesteServiceGame {
+    private static List<Jogo> listaDeJogos;
+
+    @BeforeClass
+    public static void inicializarClasse() throws URISyntaxException {
+        Path caminho = Paths.get(ClassLoader.getSystemResource("vendas-games.csv").toURI());
+        listaDeJogos = OperacoesCsv.lerCsvJogo(caminho);
+    }
+
+    @Test
+    public void testeGetJogosPs4() {
+        List<Jogo> jogosPs4 = ServiceGame.getJogosPorPlataforma(listaDeJogos, Plataformas.PS4);
+        Assert.assertEquals(5, jogosPs4.size());
+    }
+
+    @Test
+    public void testeGetJogosActivision() {
+        List<Jogo> jogosActivision = ServiceGame.getJogosPorPublisher(listaDeJogos, Publishers.ACTIVISION);
+        Assert.assertEquals(14, jogosActivision.size());
+    }
+
+}

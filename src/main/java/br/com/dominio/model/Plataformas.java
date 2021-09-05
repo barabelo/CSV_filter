6415045ab7934e2ff2f9cd846e496f6795cf88c3
Bruno <barabelo@hotmail.com>
2021-09-04 22:24:18 -0300

    Mudanças nas enums Plataformas e Publishers.

--- src/main/java/br/com/dominio/model/Plataformas.java
+++ src/main/java/br/com/dominio/model/Plataformas.java
@@ -1,25 +1,29 @@
 package br.com.dominio.model;
 
-public enum Plataformas {
-    WII ("Wii"),
-    NES ("NES"),
-    GB ("GB"),
-    DS ("DS"),
-    X360 ("X360"),
-    PS2 ("PS2"),
-    GBA ("GBA"),
-    PS4 ("PS4"),
-    PS3 ("PS3"),
-    NINTENDO_3DS ("3DS"),
-    N64 ("N64"),
-    PS ("PS"),
-    SNES ("SNES"),
-    XB ("XB"),
-    PC ("PC"),
-    ATARI_2600 ("2600"),
-    PSP ("PSP");
+import lombok.Getter;
 
+public enum Plataformas {
+    WII("Wii"),
+    NES("NES"),
+    GB("GB"),
+    DS("DS"),
+    X360("X360"),
+    PS2("PS2"),
+    GBA("GBA"),
+    PS4("PS4"),
+    PS3("PS3"),
+    NINTENDO_3DS("3DS"),
+    N64("N64"),
+    PS("PS"),
+    SNES("SNES"),
+    XB("XB"),
+    PC("PC"),
+    ATARI_2600("2600"),
+    PSP("PSP");
+
+    @Getter
     private final String valorNoCsv;
+
     Plataformas(String valorNoCsv) {
         this.valorNoCsv = valorNoCsv;
     }
--- src/main/java/br/com/dominio/model/Publishers.java
+++ src/main/java/br/com/dominio/model/Publishers.java
@@ -1,18 +1,22 @@
 package br.com.dominio.model;
 
-public enum Publishers {
-    NINTENDO ("Nintendo"),
-    MICROSOFT_GAME_STUDIOS ("Microsoft Game Studios"),
-    TAKE_TWO_INTERACTIVE ("Take-Two Interactive"),
-    SONY_COMPUTER_ENTERTAINMENT ("Sony Computer Entertainment"),
-    ACTIVISION ("Activision"),
-    UBISOFT ("Ubisoft"),
-    BETHESDA_SOFTWORKS ("Bethesda Softworks"),
-    ELECTRONIC_ARTS ("Electronic Arts"),
-    SEGA ("Sega"),
-    ATARI ("Atari");
+import lombok.Getter;
 
+public enum Publishers {
+    NINTENDO("Nintendo"),
+    MICROSOFT_GAME_STUDIOS("Microsoft Game Studios"),
+    TAKE_TWO_INTERACTIVE("Take-Two Interactive"),
+    SONY_COMPUTER_ENTERTAINMENT("Sony Computer Entertainment"),
+    ACTIVISION("Activision"),
+    UBISOFT("Ubisoft"),
+    BETHESDA_SOFTWORKS("Bethesda Softworks"),
+    ELECTRONIC_ARTS("Electronic Arts"),
+    SEGA("Sega"),
+    ATARI("Atari");
+
+    @Getter
     private final String valorNoCsv;
+
     Publishers(String valorNoCsv) {
         this.valorNoCsv = valorNoCsv;
     }

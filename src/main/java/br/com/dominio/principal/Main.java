82604cbbb5a445c59c749ec366d1eef15de6a786
Bruno <barabelo@hotmail.com>
2021-09-05 00:27:13 -0300

    Implementação da classe Main

--- src/main/java/br/com/dominio/principal/Main.java
+++ src/main/java/br/com/dominio/principal/Main.java
@@ -1,7 +1,26 @@
 package br.com.dominio.principal;
 
+import br.com.dominio.model.Jogo;
+import br.com.dominio.model.Plataformas;
+import br.com.dominio.model.Publishers;
+import br.com.dominio.service.ServiceGame;
+import br.com.dominio.utils.OperacoesCsv;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        Path caminho = null;
+        try {
+            caminho = Paths.get(ClassLoader.getSystemResource("vendas-games.csv").toURI());
+        } catch (URISyntaxException e) {
+            System.out.println("Houve um erro na abertura do CSV." + e.getMessage());
+        }
+        List<Jogo> todosOsJogos = OperacoesCsv.lerCsvJogo(caminho);
+        List<Jogo> jogosPs4 = ServiceGame.getJogosPorPlataforma(todosOsJogos, Plataformas.PS4);
+        List<Jogo> jogosActivision = ServiceGame.getJogosPorPublisher(todosOsJogos, Publishers.ACTIVISION);
     }
 }
